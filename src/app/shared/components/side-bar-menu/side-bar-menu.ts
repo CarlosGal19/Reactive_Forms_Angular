1e42999ba78666d3628456cf54efe8da00a5e1a0
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-28 16:36:55 -0600

    feat: show routes in side bar menu

--- src/app/shared/components/side-bar-menu/side-bar-menu.ts
+++ src/app/shared/components/side-bar-menu/side-bar-menu.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { reactiveRoutes } from '../../../reactive/routes/reactive.routes';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface IMenuItem {
+  title: string;
+  route: string;
+}
+
+const reactiveItems = reactiveRoutes[0].children ?? []
 
 @Component({
   selector: 'side-bar-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-bar-menu.html',
 })
 export class SideBarMenu {
 
+  reactiveMenu: IMenuItem[] = reactiveItems
+  .filter(i => i.path !== '**')
+  .map(i => ({
+    route: `reactive/${i.path}`,
+    title: `${i.title}`
+  }))
+
+  authMenu: IMenuItem[] = [
+    {
+      route: 'auth',
+      title: 'Sign up'
+    }
+  ]
+
+  countryMenu: IMenuItem[] = [
+    {
+      route: 'country',
+      title: 'Country'
+    }
+  ]
 }
