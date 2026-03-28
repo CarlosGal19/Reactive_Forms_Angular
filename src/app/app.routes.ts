80e57e78c8a144f38ebd9c51c68f5e4f731181c6
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-28 16:16:56 -0600

    feat: set up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/routes/auth.routes').then(r => r.authRoutes)
+  },
+  {
+    path: 'reactive',
+    loadChildren: () =>  import('./reactive/routes/reactive.routes').then(r => r.reactiveRoutes)
+  },
+  {
+    path: 'country',
+    loadChildren: () => import('./country/routes/country.routes').then(r => r.countryRoutes)
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive'
+  }
+];
