7b78c172edf9cf5c124b69d3b7786e57be2fb15c
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-29 17:24:12 -0600

    refactor: implement form builder

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'basic-page',
@@ -8,9 +8,17 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './basic-page.html',
 })
 export class BasicPage {
-  myForm = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(0),
-    inStock: new FormControl(0)
-  });
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStock: new FormControl(0)
+  // });
+
+  private basicFormBuilder = inject(FormBuilder);
+
+  myForm = this.basicFormBuilder.group({
+    name: [''], // [0] is initial value, [1] sync validators, [2] async validators
+    price: [0],
+    isStock: [0]
+  })
 }
