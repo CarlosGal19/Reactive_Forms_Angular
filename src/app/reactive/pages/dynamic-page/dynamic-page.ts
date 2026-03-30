3101a4febb81a0aa4f594a8e94c3e68b762a5d2e
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-29 18:38:50 -0600

    feat: show form data with json pipe

--- src/app/reactive/pages/dynamic-page/dynamic-page.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -1,11 +1,27 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form.utils';
 
 @Component({
   selector: 'dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.html',
 })
 export class DynamicPage {
+  private formBuilder = inject(FormBuilder);
 
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favorites: this.formBuilder.array([
+      ['Alien Isolation', Validators.required],
+      ['Far Cry primal', Validators.required]
+    ], [Validators.minLength(2)])
+  });
+
+  get favoritesGames() {
+    return this.myForm.get('favorites') as FormArray;
+  }
 }
