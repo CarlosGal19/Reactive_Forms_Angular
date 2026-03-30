677b372b5eb4b579bfd5c2df91000592063e36e0
Carlos Galindo <c.galindo.islas@gmail.com>
2026-03-29 19:05:57 -0600

    refactor: show error message in each favorite game input

--- src/app/utils/form.utils.ts
+++ src/app/utils/form.utils.ts
@@ -1,16 +1,9 @@
-import { FormGroup } from "@angular/forms";
+import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
 
 export class FormUtils {
-  static isValidField(form: FormGroup, fieldName: 'name' | 'price' | 'inStock'): boolean | null {
-    return (
-      !!form.controls[fieldName].errors &&
-      form.controls[fieldName].touched);
-  }
 
-  static getFieldError(form: FormGroup, fieldName: 'name' | 'price' | 'inStock'): string | null {
-    const errors =  form.controls[fieldName].errors ?? {};
-
-    for(const key of Object.keys(errors)) {
+  static getTextErrors(errors: ValidationErrors) {
+    for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
           return 'This field is required'
@@ -20,7 +13,31 @@ export class FormUtils {
           return `Minimum value of ${errors['min'].min}`
       }
     }
-
     return null;
+  }
+
+  static isValidField(form: FormGroup, fieldName: 'name' | 'price' | 'inStock'): boolean | null {
+    return (
+      !!form.controls[fieldName].errors &&
+      form.controls[fieldName].touched);
+  }
+
+  static getFieldError(form: FormGroup, fieldName: 'name' | 'price' | 'inStock'): string | null {
+    const errors = form.controls[fieldName].errors ?? {};
+    return this.getTextErrors(errors);
+  }
+
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      !!formArray.controls[index].errors &&
+      formArray.controls[index].touched
+    )
+  }
+
+  static getFieldErrorInArray(form: FormGroup, index: number): string | null {
+    const formArray = form.controls['favorites'] as FormArray;
+    const errors = formArray.controls[index].errors ?? {};
+
+    return this.getTextErrors(errors);
   }
 }
